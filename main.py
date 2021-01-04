6021332c781d3600531ffb0a0937f75543ffb0ec
bamblebam <burhanizrangwala@gmail.com>
2021-01-04 14:39:49 +0530

    feature engineering

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # %%
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+# %%
+df = pd.read_csv("./datasets/london_merged.csv",
+                 parse_dates=['timestamp'], index_col='timestamp')
+df.head()
+# %%
+df['hour'] = df.index.hour
+df['day_of_week'] = df.index.dayofweek
+df['day_of_month'] = df.index.day
+df['month'] = df.index.month
+df.head()
+# %%

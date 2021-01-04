bd0bec88b421406b428e1e6490d999c4a5d34325
bamblebam <burhanizrangwala@gmail.com>
2021-01-04 14:55:29 +0530

    data visualization

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from pandas.plotting import register_matplotlib_converters
+from matplotlib import rcParams
 
 register_matplotlib_converters()
 RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
+rcParams['figure.figsize'] = 22, 10
 
 # %%
 df = pd.read_csv("./datasets/london_merged.csv",
@@ -23,4 +25,15 @@ df['day_of_week'] = df.index.dayofweek
 df['day_of_month'] = df.index.day
 df['month'] = df.index.month
 df.head()
+# %%
+sns.lineplot(x=df.index, y='cnt', data=df)
+# %%
+df_month = df.resample('M').sum()
+sns.lineplot(x=df_month.index, y='cnt', data=df_month)
+# %%
+sns.pointplot(x='hour', y='cnt', data=df)
+# %%
+sns.pointplot(x='hour', y='cnt', data=df, hue='is_holiday')
+# %%
+sns.pointplot(x='day_of_week', y='cnt', data=df)
 # %%
